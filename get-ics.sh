14f2614b29b472571231cb49f0d9dc39d6247d26
Nikolas Kuschnig <nkuschnig@disroot.org>
2025-11-09 12:05:46 +1100

    Try and fix the workflow

--- get-ics.sh
+++ get-ics.sh
@@ -1,3 +1,3 @@
-curl https://calendar.google.com/calendar/ical/2rnbhi06i37jfdmprrls497vr8%40group.calendar.google.com/public/basic.ics > ics/monash-ebs.ics
-curl https://calendar.google.com/calendar/ical/monash.edu_oh4b3l045bt5nm75tnlnt1gv5s%40group.calendar.google.com/public/basic.ics > ics/monash-econ.ics
-curl https://calendar.google.com/calendar/ical/kifge5897trj5b966sf1me6d1o%40group.calendar.google.com/public/basic.ics > ics/unimelb-ebe.ics
+curl https://calendar.google.com/calendar/ical/2rnbhi06i37jfdmprrls497vr8%40group.calendar.google.com/public/basic.ics > public/ics/monash-ebs.ics
+curl https://calendar.google.com/calendar/ical/monash.edu_oh4b3l045bt5nm75tnlnt1gv5s%40group.calendar.google.com/public/basic.ics > public/ics/monash-econ.ics
+curl https://calendar.google.com/calendar/ical/kifge5897trj5b966sf1me6d1o%40group.calendar.google.com/public/basic.ics > public/ics/unimelb-ebe.ics
